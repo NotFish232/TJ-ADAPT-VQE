446d90bc2db54415c34d4121481ccee6c7b3df73
Japneet <japneet691@gmail.com>
2025-02-12 22:10:21 -0500

    new file:   tj_adapt_vqe/vqe/analysis.py

---
+++ tj_adapt_vqe/vqe/analysis.py
@@ -0,0 +1,11 @@
+def analysis():
+    """we run this after running the pools 
+    1. recreate the molecule
+    2. load the data u saved
+    3. make the data look pretty so u can analyze it 
+        i.e. create list of (operators, coefficients)
+    4. start w/the hartree fock state
+    5. apply ops to hf state 
+    6. get final wavefunction 
+    """
+analysis()
