e71ee0e662cf81fe07a55fb9b76258869cb80d40
Marina <marinalin2007@gmail.com>
2025-05-07 13:45:30 -0400

    trust region, cobyla, etc

--- tj_adapt_vqe/optimizers/__init__.py
+++ tj_adapt_vqe/optimizers/__init__.py
@@ -1,6 +1,6 @@
 from .adam import Adam
 from .lbfgs import LBFGS
-from .levenberg_marquardt import LevenbergMarquardt
+# from .levenberg_marquardt import LevenbergMarquardt
 from .optimizer import Optimizer
 from .sgd import SGD
 
@@ -9,5 +9,4 @@ __all__ = [
     "LBFGS",
     "Optimizer",
     "SGD",
-    "LevenbergMarquardt",
 ]
