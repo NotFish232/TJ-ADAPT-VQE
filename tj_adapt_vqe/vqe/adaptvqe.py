eef81f8038461733527ddc8051d8ff84b5eefd6a
Justin Lee <justin.adam.lee@gmail.com>
2025-05-08 15:05:58 -0400

    comment adapt vqe file

--- tj_adapt_vqe/vqe/adaptvqe.py
+++ tj_adapt_vqe/vqe/adaptvqe.py
@@ -18,9 +18,11 @@ from .vqe import VQE
 
 class ADAPTConvergenceCriteria(Enum):
     """
-    Criteria used to evaluate when to stop the ADAPT-VQE algorithm
-    Possible criteria includes Gradient, whether the max gradient of any pool operators is below a threshold
-    Or whether not enough improvement has been made in successive iterations.
+    Inherits from `enum.Enum`. An enum representing the different convergence criteria for the ADAPT algorithm.
+
+    Members:
+        `ADAPTConvergenceCriteria.Gradient` conoverged if gradient of selected operator falls below a threshold.
+        `ADAPTConvergenceCriteria.LackOfImprovement` converged if change in energy betwen iterations falls below a threshold.
     """
 
     Gradient = 0
@@ -29,7 +31,8 @@ class ADAPTConvergenceCriteria(Enum):
 
 class ADAPTVQE(VQE):
     """
-    Class implementing the ADAPT-VQE algorithm
+    Inherits from `VQE`. Implements the ADAPT VQE algorithm which selects new operators for the ansatz
+    based on the operator with the highest gradient within the pool.
     """
 
     def __init__(
@@ -43,15 +46,19 @@ class ADAPTVQE(VQE):
         conv_threshold: float = 0.01,
     ) -> None:
         """
-        Initializes the ADAPTVQE object
+        Constructs an instance of `ADAPTVQE`. Calls `super().__init__()` with the passed
+        `molecule`, `optimizer`, `observables`, and `num_shots`. Calculates the commutator
+        of each operator within the passed pool and the molecular hamiltonian.
 
         Args:
-            molecule: MolecularData, the molecular data that is used for the adapt vqe,
-            pool: Pool, the pool that the ADAPTVQE uses to form the Ansatz,
-            adapt_conv_criteria: ADAPTConvergenceCriteria = ADAPTConvergenceCriteria.Gradient, the criteria for
-            when to terminate adaptive iterations, see the actual Enum impl for information about each criteria,
-            conv_threshold: float = 0.01, the threshold wrt to the adapt_conv_criteria to stop adaptive iterations,
-            Other arguments are passed directly to the VQE constructor
+            self (Self): A reference to the current class instance.
+            molecule (MolecularData): The molecule that the ADAPTVQE algorithm will be ran on.
+            pool (Pool): The pool to select operators from.
+            optimizer (Optimizer): The optimizer to perform each VQE iteration on. Passed to super class.
+            observables (list[Observable], optional): The observables to track. Passed to super class. Defaults to [].
+            num_shots (int, optional): The num shots for simulations. Passed to super class. Defaults to 1024.
+            adapt_conv_criteria (ADAPTConvergenceCriteria, optional): The criteria to use for ADAPT convergence. Defaults to ADAPTConvergenceCriteria.Gradient.
+            conv_threshold (float, optional): The threshold that the criteria uses to determine ADAPT convergence. Defaults to 0.01.
         """
 
         self.adapt_vqe_it = 1
@@ -70,8 +77,17 @@ class ADAPTVQE(VQE):
     @override
     def _make_ansatz(self: Self) -> QuantumCircuit:
         """
-        Overides the VQE ansatz by starting it unparameterized
+        Overrides the VQE implementation of `_make_ansatz(...)` to create a simpler ansatz
+        that works better with ADAPTVQE and selecting operators dynamically. Can either be set
+        to the perfect pair configuration or the hartfree-fock state.
+
+        Args:
+            self (Self): A reference to the current class instance.
+
+        Returns:
+            QuantumCircuit: The initial ansatz for the ADAPTVQE algorithm to use.
         """
+
         ansatz = make_perfect_pair_ansatz(self.n_qubits)
 
         return ansatz
@@ -79,7 +95,14 @@ class ADAPTVQE(VQE):
     @override
     def _make_progress_description(self: Self) -> str:
         """
-        Overrides the VQE progress_description including all of its params along with ADAPTVQE specific details
+        Overrides the VQE implementation of `_make_progress_description(...)` to create a progress bar with
+        more information. Includes the implementation of `super()._make_progress_description(...)` in its output.
+
+        Args:
+            self (Self): A reference to the current class instance.
+
+        Returns:
+            str: A string representing the description of the tqdm progress bar.
         """
 
         vqe_progress_descrption = super()._make_progress_description()
@@ -90,10 +113,18 @@ class ADAPTVQE(VQE):
 
     def _calculate_commutators(self: Self) -> tuple[list[Observable], list[int]]:
         """
-        Calculates the commutator between the Hamiltonian and every Observable
-        within the provided Pool. Also returnns the "density" for each operator, which is how many actual operators
-        each operator within the pool represents. For instance, pool.get_op() could return an array of length 3
-        with 3 actual operators that get fused together in poolget_exp_op() but gradients should be calculated seperately
+        Calculates the commutator between `self.hamiltonian` and every Observable in `self.pool`.
+        Also returns the "density" for each operator, which is how many matrix operators
+        each qiskit operator within the pool represents. For instance, `pool.get_op(...)`
+        could return an array of length 3 with 3 actual operators that get fused together in `pool.get_exp_op(...)`
+        with gradients calculated seperately but only one qiskit circuit.
+
+
+        Args:
+            self (Self): A reference to the current class instance.
+
+        Returns:
+            tuple[list[Observable],list[int]]: A list of Observable wrapped commutators and a list of the count of each operator
         """
 
         # if pool only has one operator then clearly commutators aren't necessary
@@ -125,8 +156,16 @@ class ADAPTVQE(VQE):
 
     def _find_best_operator(self: Self) -> tuple[float, int]:
         """
-        Returns the gradient and idx of the best operator  within the pool that maximizes the commutator with the hamiltonian
+        Finds the best operator in the pool by calculating the expectation value of each operator's commutator
+        and finding the operator that has the largest absolute value of the expectation value. If an operator
+        corresponds to multiple matrix operators, the sum of the absolute values of expectation values will be
+        substituted for the singular expectation value.
 
+        Args:
+            self (Self): A reference to the current class instance.
+
+        Returns:
+            tuple[float,int]: A tuple of the highest gradient and its corresponding pool operator index.
         """
 
         # again pool only has one operator, so just return idx 0
@@ -158,7 +197,15 @@ class ADAPTVQE(VQE):
 
     def _is_converged(self: Self) -> bool:
         """
-        Checks whether ADAPT VQE is converged and should exist, is based on the adapt_conv_criteria and conv_threhsold
+        Checks whether the ADAPT-VQE algorithm is converged. Utilizes information that has been logged
+        to `logger.logged_values`. Uses `self.adapt_conv_criteria` alongside `self.conv_threshold` to decide
+        whether the algorithm is converged.
+
+        Args:
+            self (Self): A reference to the current class instance.
+
+        Returns:
+            bool: Whether ADAPT VQE has converged.
         """
 
         if self.adapt_conv_criteria == ADAPTConvergenceCriteria.Gradient:
@@ -173,9 +220,20 @@ class ADAPTVQE(VQE):
 
             return abs(adapt_energies[-1] - adapt_energies[-2]) < self.conv_threshold
 
-        raise NotImplementedError()
-
     def _prepare_new_op(self: Self, idx: int) -> QuantumCircuit:
+        """
+        Prepares an operator to be added to the quantum circuit. This includes
+        renaming parameters to ensure uniqueness and turning qiskit `Gate` instances
+        to `QuantumCircuit` instances.
+
+        Args:
+            self (Self): A reference to the current class instance.
+            idx (int) The idx of the operator in the pool that should be prepared.
+
+        Returns:
+            QuantumCircuit: The circuit representing the operator.
+        """
+
         op = self.pool.get_exp_op(idx)
         label = self.pool.get_label(idx)
 
@@ -185,10 +243,14 @@ class ADAPTVQE(VQE):
 
         return op
 
-
     def run(self: Self) -> None:
         """
-        Runs the ADAPT-VQE Algorithm
+        Runs the ADAPT VQE algorithm. Each iteration, a new operator is chosen, appened to the end
+        of the ansatz, `super().run(...)` is called to optimize VQE parameters, and then repeated until convergence.
+        Through this process, values are also logged to `self.logger`.
+
+        Args:
+            self (Self): A reference to the current class instance.
         """
 
         # creates progress bar if not created
@@ -210,7 +272,6 @@ class ADAPTVQE(VQE):
             if self._is_converged():
                 break
 
-                
             new_op = self._prepare_new_op(max_idx)
             self.circuit.compose(new_op, inplace=True)
             self.transpiled_circuit = self._transpile_circuit(self.circuit)
