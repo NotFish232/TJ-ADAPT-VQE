73e8d1b8801489571b0f950c405020f61db21053
Justin Lee <justin.adam.lee@gmail.com>
2025-07-04 23:48:29 -0400

    better structure + time speed ups

--- tj_adapt_vqe/post_process.py
+++ tj_adapt_vqe/post_process.py
@@ -1,17 +1,21 @@
 import json
 import re
-from functools import reduce
+from functools import lru_cache, reduce
 from itertools import product
 from pathlib import Path
 
 import matplotlib.pyplot as plt
 import mlflow
+from matplotlib.figure import Figure
+from mlflow.entities import Run
+from mlflow.store.entities import PagedList
+from mlflow.tracking import MlflowClient
 from typing_extensions import Any
 
 RUN_DIR = "./runs/"
 mlflow.set_tracking_uri(RUN_DIR)
 
-OUT_DIR = "./results"
+RESULTS_DIR = "./results"
 
 
 CAPITALIZATION_RULES = [
@@ -21,6 +25,121 @@ CAPITALIZATION_RULES = [
     ("gsd", "GSD"),
     ("qeb", "QEB"),
 ]
+
+
+@lru_cache(maxsize=None)
+def get_runs() -> PagedList[Run]:
+    """
+    Simple wrapper around fetching all runs that allows it to be lru_cache'd.
+
+    Returns:
+        PagedList[Run]: All runs
+    """
+    client = MlflowClient()
+
+    return client.search_runs(experiment_ids=["0"])
+
+
+@lru_cache(maxsize=None)
+def get_run_params(run_id: str) -> dict[str, Any]:
+    """
+    Retrieves all parameters associated with a mlflow run ID.
+    Performs the necessary processing on those params
+
+    Args:
+        run_id (str): The MLflow run ID.
+
+    Returns:
+        dict[str, list[tuple[int, float]]]: dictionary mapping each metric name
+        to a sorted list of (step, value) tuples.
+
+    """
+    client = MlflowClient()
+    raw_params = client.get_run(run_id).data.params
+
+    params = {}
+
+    for param in raw_params:
+        params[param] = raw_params[param]
+
+        # convert json compatible params into json
+        try:
+            params[param] = json.loads(params[param])
+        except ValueError:
+            pass
+
+    params["starting_ansatz"] = " ".join(params["starting_ansatz"])
+    if "pool" not in params:
+        params["pool"] = {"name": params["starting_ansatz"][1]}
+
+    return params
+
+
+@lru_cache(maxsize=None)
+def get_run_metrics(run_id: str) -> dict[str, Any]:
+    """
+    Retrieves all logged metric histories from a given MLflow run ID.
+    Each metric's values are sorted by step to help with plotting.
+
+    Args:
+        run_id (str): The MLflow run ID.
+
+    Returns:
+        dict[str, list[tuple[int, float]]]: dictionary mapping each metric name
+        to a sorted list of (step, value) tuples.
+
+    """
+    client = MlflowClient()
+    raw_metrics = client.get_run(run_id).data.metrics
+
+    metrics = {}
+
+    for metric in raw_metrics:
+        history = client.get_metric_history(run_id, metric)
+        sorted_history = sorted(
+            [(h.step, h.value) for h in history], key=lambda x: x[0]
+        )
+        metrics[metric] = sorted_history
+
+    # some metrics have to be processed
+
+    # make n params, cnot count, and circuit depth same length as energy
+    for metric in ["n_params", "cnot_count", "circuit_depth"]:
+        if metric in metrics:
+            metrics[metric].extend(
+                metrics[metric][-1]
+                for _ in range(len(metrics["energy"]) - len(metrics[metric]))
+            )
+
+    return metrics
+
+
+def check_filtered(
+    params: dict[str, Any], filter_fixed: dict[str, Any], filter_ignored: dict[str, Any]
+) -> bool:
+    """
+    Check if run should be filtered away or not. Implements the logic used for filter_fixed / filter_ignored.
+
+    Args:
+        params (dict[str, Any]): The params of the run.
+         filter_fixed (dict[str, Any]): Dictionary of fixed parameters to filter by.
+        filter_ignored (dict[str, list[Any]]): Dictionary of parameters that if satisifed should be ignored.
+
+    Returns:
+        bool: If the run should be filtered away.
+    """
+
+    # filter for fixed values
+    for key, val in filter_fixed.items():
+        if get_nested_json(params, key) != val:
+            return True
+
+    # Filter for ignored values
+    for key, val in filter_ignored.items():
+        if get_nested_json(params, key) in val:
+            return True
+
+    return False
 
 
 def get_nested_json(data: dict[str, Any], key: str) -> Any:
@@ -37,47 +156,6 @@ def get_nested_json(data: dict[str, Any], key: str) -> Any:
     """
 
     return reduce(lambda x, y: None if x is None else x.get(y), key.split("."), data)  # type: ignore
-
-
-def get_logged_metrics(run_id: str):
-    """
-    Retrieves all logged metric histories from a given MLflow run ID.
-    Each metric's values are sorted by step to help with plotting.
-
-    Args:
-        run_id (str): The MLflow run ID.
-
-    Returns:
-        dict[str, list[tuple[int, float]]]: dictionary mapping each metric name
-        to a sorted list of (step, value) tuples.
-
-    Currently found metrics: ['n_params', 'adapt_energy', 'number_observable', 'avg_grad',
-    'energy', 'energy_percent', 'spin_squared_observable', 'energy_percent_log',
-    'adapt_operator_grad', 'max_grad', 'adapt_operator_idx', 'spin_z_observable']
-    """
-    client = mlflow.tracking.MlflowClient()
-    data = client.get_run(run_id).data
-
-    metrics = {}
-
-    for k in data.metrics:
-        history = client.get_metric_history(run_id, k)
-        sorted_history = sorted(
-            [(h.step, h.value) for h in history], key=lambda x: x[0]
-        )
-        metrics[k] = sorted_history
-
-    # some metrics have to be processed
-
-    # make n params, cnot count, and circuit depth same length as energy
-    for metric in ["n_params", "cnot_count", "circuit_depth"]:
-        if metric in metrics:
-            metrics[metric].extend(
-                metrics[metric][-1]
-                for _ in range(len(metrics["energy"]) - len(metrics[metric]))
-            )
-
-    return metrics
 
 
 def adjust_capitalization(s: str) -> str:
@@ -102,106 +180,74 @@ def adjust_capitalization(s: str) -> str:
 
 def compare_runs(
     *,
+    group_by: str,
     x_parameter: str | None = None,
     y_parameter: str,
-    title: str | None = None,
-    x_axis_title: str | None = None,
-    y_axis_title: str | None = None,
-    group_by: str,
+    title: str = "",
+    x_axis_title: str = "",
+    y_axis_title: str = "",
     filter_fixed: dict[str, Any] = {},
     filter_ignored: dict[str, list[Any]] = {},
     log_scale: bool = False,
-):
+) -> Figure:
     """
     Comparing multiple runs grouped by a specified parameter, fixed by a specific filter, and with specific x and y axis.
 
     Args:
-        x_parameter (str | None): The parameter for the x axis. Defaults to None.
-        y_parameter (str): The parameter to actually plot on the graph, like energy_percent_log.
-        title: (str | None): The title for the graph. Defaults to None.
-        x_axis_title: (str | None): The x axis title. Defaults to None.
-        y_axis_title: (str | None): The y axis title. Defaults to None.
         group_by (str): Parameter name to group runs by (e.g., "optimizer"). Dependent Variable.
+        x_parameter (str | None): The parameter for the x axis. Defaults to None.
+        y_parameter (str): The parameter to actually plot on the graph, like energy_percent.
+        title: (str): The title for the graph. Defaults to "".
+        x_axis_title: (str): The x axis title. Defaults to "".
+        y_axis_title: (str): The y axis title. Defaults to "".
         filter_fixed (dict[str, Any]): Dictionary of fixed parameters to filter by. The constant stuff. Defaults to {}.
         filter_ignored (dict[str, list[Any]]): Dictionary of parameters that if satisifed should be ignored. Useful if you want
         to remove some runs from a plot while keeping others. Defaults to {}.
         log_scale (bool): Whether to use log scale on y axis. Defaults to False.
 
     Returns:
-        Matplotlib plot.
+        Figure: Matplotlib plot.
     """
-    client = mlflow.tracking.MlflowClient()
-    runs = client.search_runs(experiment_ids=["0"])
+    runs = get_runs()
 
     grouped_runs = {}  # type: ignore
 
     for run in runs:
         run_id = run.info.run_id
-        params = run.data.params
+        params = get_run_params(run_id)
 
-        for key in params:
-            # convert json compatible params into json
-            try:
-                params[key] = json.loads(params[key])
-            except ValueError:
-                pass
-
-        if "pool" not in params:
-            params["pool"] = {"name": params["starting_ansatz"][1]}
-        params["starting_ansatz"] = " ".join(params["starting_ansatz"])
-
-        # Filter for fixed values
-        skip = False
-        for key, val in filter_fixed.items():
-            if get_nested_json(params, key) != val:
-                skip = True
-                break
-        for key, val in filter_ignored.items():
-            if get_nested_json(params, key) in val:
-                skip = True
-                break
-
-        if skip:
+        # check whether to use or skip this run
+        if check_filtered(params, filter_fixed, filter_ignored):
             continue
 
         # Group by selected parameter
-        group_val = get_nested_json(params, group_by)
-
-        if group_val is None:
-            continue
-
-        grouped_runs.setdefault(group_val, []).append(run_id)
-
-    sorted_runs = sorted(grouped_runs.items())
+        if (group_val := get_nested_json(params, group_by)) is not None:
+            grouped_runs.setdefault(group_val, []).append(run_id)
 
     fig, ax = plt.subplots(constrained_layout=True, figsize=(19.20, 10.80))
 
-    for group, run_ids in sorted_runs:
+    for group, run_ids in sorted(grouped_runs.items()):
         for run_id in run_ids:
-            metrics = get_logged_metrics(run_id)
+            metrics = get_run_metrics(run_id)
             if y_parameter not in metrics:
                 continue
 
-            x_values, y_values = zip(*metrics[y_parameter])
+            x_vals, y_vals = zip(*metrics[y_parameter])
 
             if x_parameter is not None:
                 if x_parameter not in metrics:
                     continue
 
-                _, x_values = zip(*metrics[x_parameter])
+                _, x_vals = zip(*metrics[x_parameter])
 
-            l = plt.plot(
-                x_values,
-                y_values,
-                marker="o",
-                label=adjust_capitalization(group),
-            )
+            l = plt.plot(x_vals, y_vals, marker="o", label=adjust_capitalization(group))
 
-            if x_parameter is None:  # plot error bar for each adapt iteration
+            if x_parameter is None:
+                # plot error bar for each adapt iteration if x parameter is just time
                 for i, (t, n_param) in enumerate(metrics["n_params"]):
                     if i == 0 or n_param != metrics["n_params"][i - 1]:
-                        x_i = x_values[t - 1]
-                        y_i = y_values[t - 1]
+                        x_i = x_vals[t - 1]
+                        y_i = y_vals[t - 1]
 
                         if log_scale:
                             err_y1 = y_i / 10
@@ -224,22 +270,12 @@ def compare_runs(
             label="Chemical Accuracy",
         )
 
-    formatted_x_parameter = (
-        adjust_capitalization(x_parameter) if x_parameter is not None else "Iterations"
-    )
-    formatted_y_pararmeter = adjust_capitalization(y_parameter)
-    formatted_group = adjust_capitalization(group_by.split(".")[0])
-
-    plt.title(
-        title
-        or f"{formatted_y_pararmeter} vs {formatted_x_parameter} (Grouped by {formatted_group})",
-        fontsize=24,
-    )
-    plt.xlabel(x_axis_title or formatted_x_parameter, fontsize=21)
-    plt.ylabel(y_axis_title or formatted_y_pararmeter, fontsize=21)
-    plt.xticks(fontsize=18)
-    plt.yticks(fontsize=18)
-    plt.legend(fontsize=21)
+    plt.title(title, fontsize=32)
+    plt.xlabel(x_axis_title, fontsize=29)
+    plt.ylabel(y_axis_title, fontsize=29)
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+    plt.legend(fontsize=29)
 
     if log_scale:
         ax.set_yscale("log")
@@ -277,11 +313,11 @@ def main() -> None:
         # graphs for pools
         for optimizer in optimizers:
             fig = compare_runs(
+                group_by="pool.name",
                 y_parameter="energy_percent",
                 title=f"Energy Error with {adjust_capitalization(optimizer)} on {molecule_name} ({molecule_basis})",
-                x_axis_title="VQE Iteration",
+                x_axis_title="Cumulative VQE Iterations",
                 y_axis_title="Energy Error in a.u",
-                group_by="pool.name",
                 filter_fixed={
                     "optimizer.name": optimizer,
                     "qiskit_backend.shots": 0,
@@ -290,18 +326,18 @@ def main() -> None:
                 log_scale=True,
             )
 
-            Path(f"{OUT_DIR}/pools/{molecule}").mkdir(parents=True, exist_ok=True)
-            fig.savefig(f"{OUT_DIR}/pools/{molecule}/{optimizer}.png")
+            Path(f"{RESULTS_DIR}/pools/{molecule}").mkdir(parents=True, exist_ok=True)
+            fig.savefig(f"{RESULTS_DIR}/pools/{molecule}/{optimizer}.png")
             plt.close(fig)
 
         # for each other metric
         for metric in metrics:
             fig = compare_runs(
+                group_by="pool.name",
                 y_parameter=metric,
                 title=f"{adjust_capitalization(metric)} with Cobyla on {molecule_name} ({molecule_basis})",
-                x_axis_title="VQE Iteration",
+                x_axis_title="Cumulative VQE Iterations",
                 y_axis_title=adjust_capitalization(metric),
-                group_by="pool.name",
                 filter_fixed={
                     "optimizer.name": "cobyla_optimizer",
                     "qiskit_backend.shots": 0,
@@ -309,18 +345,18 @@ def main() -> None:
                 },
             )
 
-            Path(f"{OUT_DIR}/pools/{molecule}").mkdir(parents=True, exist_ok=True)
-            fig.savefig(f"{OUT_DIR}/pools/{molecule}/{metric}.png")
+            Path(f"{RESULTS_DIR}/pools/{molecule}").mkdir(parents=True, exist_ok=True)
+            fig.savefig(f"{RESULTS_DIR}/pools/{molecule}/{metric}.png")
             plt.close(fig)
 
         # graphs for optimizers
         for pool, backend in product(pools, backends):
             fig = compare_runs(
+                group_by="optimizer.name",
                 y_parameter="energy_percent",
                 title=f"Energy Error with {adjust_capitalization(pool)} on {molecule_name} ({molecule_basis}) ({backend})",
-                x_axis_title="VQE Iteration",
+                x_axis_title="Cumulative VQE Iterations",
                 y_axis_title="Energy Error in a.u",
-                group_by="optimizer.name",
                 filter_fixed={
                     "pool.name": pool,
                     "qiskit_backend.shots": 0 if backend == "exact" else 2**20,
@@ -334,20 +370,20 @@ def main() -> None:
                 log_scale=True,
             )
 
-            Path(f"{OUT_DIR}/optimizers/{molecule}/{backend}").mkdir(
+            Path(f"{RESULTS_DIR}/optimizers/{molecule}/{backend}").mkdir(
                 parents=True, exist_ok=True
             )
-            fig.savefig(f"{OUT_DIR}/optimizers/{molecule}/{backend}/{pool}.png")
+            fig.savefig(f"{RESULTS_DIR}/optimizers/{molecule}/{backend}/{pool}.png")
             plt.close(fig)
 
         # graphs for observables
         for observable in observables:
             fig = compare_runs(
+                group_by="optimizer.name",
                 y_parameter=observable,
                 title=f"{adjust_capitalization(observable)} with Cobyla and FSD on {molecule_name} ({molecule_basis})",
-                x_axis_title="VQE Iteration",
+                x_axis_title="Cumulative VQE Iterations",
                 y_axis_title="Expectation value",
-                group_by="optimizer.name",
                 filter_fixed={
                     "optimizer.name": "cobyla_optimizer",
                     "pool.name": "fsd_pool",
@@ -356,8 +392,10 @@ def main() -> None:
                 },
             )
 
-            Path(f"{OUT_DIR}/observables/{molecule}").mkdir(parents=True, exist_ok=True)
-            fig.savefig(f"{OUT_DIR}/observables/{molecule}/{observable}.png")
+            Path(f"{RESULTS_DIR}/observables/{molecule}").mkdir(
+                parents=True, exist_ok=True
+            )
+            fig.savefig(f"{RESULTS_DIR}/observables/{molecule}/{observable}.png")
             plt.close(fig)
 
 
